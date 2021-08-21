6e6be942a0be6b146ed8028c5954b0c8f4320e32
Justin Heisler <justinheisler@Justins-MacBook-Pro.local>
2021-08-21 16:03:41 -0600

    testing app name'

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,11 @@
-console.log('cli puppy message')
+const { execSync} = require('child_process')
+const path = require('path')
+const fs = require('fs')
+
+if (process.argv.length < 3) {
+  console.log('You have to provide a name dumb dumb')
+  return process.exit(1)
+}
+
+console.log('you used a name')
+process.exit(1)
