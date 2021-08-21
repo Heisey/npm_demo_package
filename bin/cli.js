785a240a1ba85af8b2095e17a7c535996157bd41
Justin Heisler <justinheisler@Justins-MacBook-Pro.local>
2021-08-21 16:21:29 -0600

    progess check

--- bin/cli.js
+++ bin/cli.js
@@ -3,10 +3,31 @@ const { execSync} = require('child_process')
 const path = require('path')
 const fs = require('fs')
 
+const { projectInstall } = require('./projectInstall')
+
 if (process.argv.length < 3) {
-  console.log('You have to provide a name dumb dumb')
+  console.log('You have to provide a project name ')
+  console.log('For example...')
+  console.log('npx npm_register_test your_app_name')
   return process.exit(1)
 }
 
-console.log('you used a name')
-process.exit(1)
+const projectName = process.argv[2]
+const currentPath = process.cwd()
+const projectPath = path.join(currentPath, projectName)
+const repo = console.log('cloning repo')
+
+
+
+try {
+  fs.mkdirSync(projectPath)
+} catch (err) {
+  if (err.code === 'EEXIST') {
+    console.log('Project Name already exists in current dir, try another name')
+  } else {
+    console.log(err)
+  }
+  process.exit(1)
+}
+
+projectInstall()
---
+++ bin/projectInstall.js
@@ -0,0 +1,14 @@
+
+const projectInstall = async () => {
+  try {
+    console.log('Downloading files')
+    repo
+
+
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+module.exports.projectInstall = projectInstall
